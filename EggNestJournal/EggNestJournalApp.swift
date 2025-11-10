33bab9faeba95c94a4188ef742c9eaa109ef3d9e
esplusnin <esplusnin94@yandex.ru>
2025-11-10 08:47:44 +0300

    Initial Commit

---
+++ EggNestJournal/EggNestJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggNestJournalApp.swift
+//  EggNestJournal
+//
+//  Created by Евгений on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct EggNestJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
